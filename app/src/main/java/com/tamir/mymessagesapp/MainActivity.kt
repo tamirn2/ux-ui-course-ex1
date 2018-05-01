e39a2c8e76e9c59713e32d330c292d1170fa2afd
tamir <tamir.nahum@mail.huji.ac.il>
2018-05-01 17:34:26 +0300

    kotlin

--- app/src/main/java/com/tamir/mymessagesapp/MainActivity.kt
+++ app/src/main/java/com/tamir/mymessagesapp/MainActivity.kt
@@ -3,10 +3,15 @@ package com.tamir.mymessagesapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : MainFragment.OnFragmentInteractionListener,
+                     AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.activity_main_frame_layout, MainFragment())
+                .commit()
     }
 }
--- app/src/main/java/com/tamir/mymessagesapp/MainFragment.kt
+++ app/src/main/java/com/tamir/mymessagesapp/MainFragment.kt
@@ -24,28 +24,18 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        val a = 6
         return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
     }
 
     override fun onAttach(context: Context) {
@@ -73,10 +63,7 @@ class MainFragment : Fragment() {
      * (http://developer.android.com/training/basics/fragments/communicating.html)
      * for more information.
      */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
+    interface OnFragmentInteractionListener {}
 
     companion object {
         /**
