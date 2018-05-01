4be82abe0281ffc44b3bdf0c10a72c13600ead7a
tamir <tamir.nahum@mail.huji.ac.il>
2018-05-01 17:58:19 +0300

    kotlin

--- app/src/main/java/com/tamir/mymessagesapp/MainActivity.kt
+++ app/src/main/java/com/tamir/mymessagesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tamir.mymessagesapp
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +9,7 @@ class MainActivity : MainFragment.OnFragmentInteractionListener,
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR)
         supportFragmentManager
                 .beginTransaction()
                 .add(R.id.activity_main_frame_layout, MainFragment())
